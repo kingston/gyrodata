ebea8db4eef25a11d064eb007fb02b7d60c30a5a
Kingston Tam <web@kingstontam.com>
2013-12-04 20:38:50 -0800

    Add min records filter

--- visualizers/data.py
+++ visualizers/data.py
@@ -1,39 +1,13 @@
 # Library for accessing gyro data
-import os, csv
+import os, csv, sys
 
-def getAttributeList():
-    return ['activity', 'gender', 'age', 'height', 'weight', 'position', 'activityFolder', 'person', 'accfile', 'gyrofile']
-
-def writeMetadata(data, path):
-    with open(path, 'wb') as csvfile:
-        metawriter = csv.writer(csvfile, delimiter=',')
-        for entry in data:
-            record = []
-            for attr in getAttributeList():
-                record.append(entry[attr])
-            metawriter.writerow(record)
-
-def readMetadata(path):
-    data = []    
-    with open(path, 'r') as csvfile:
-        metareader = csv.reader(csvfile)
-        for row in metareader:
-            data.append(dict(zip(getAttributeList(), row)))
-    return data
+# import gyrodata from parent directory
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+import gyrodata
 
 def readNumericData(path):
-    data = readCsvData("../" + path)
+    data = gyrodata.readCsvData("../" + path)
     return [[float(x or 0) for x in l] for l in data]
-
-def readCsvData(path):
-    with open(path, 'r') as csvfile:
-        csvreader = csv.reader(csvfile, delimiter=',')
-        return list(csvreader)
-
-def writeCsvData(data, path):
-    with open(path, 'wb') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=',')
-        csvwriter.writerows(data)
 
 def getEntryById(meta, id):
     for entry in meta:
--- visualizers/process.py
+++ visualizers/process.py
@@ -1,5 +1,8 @@
 import data
 import numpy as np
+import scipy
+from scipy import integrate, signal
+import math
 
 def filterByTime(start, end, data):
     return data[(data[:, 0] >= start) & (data[:, 0] <= end)]
@@ -9,13 +12,41 @@ def normalizeDatasets(accData, gyroData):
     end = min(accData[:, 0].max(), gyroData[:, 0].max())
     return (filterByTime(start, end, accData), filterByTime(start, end, gyroData))
 
-def absoluteReferenceAccData(accData, gyroData):
+def processData(accData, gyroData):
+    return (accData, gyroData)
+    # reinterpolate acceleration data into even samples
+    minTime = accData[:, 0].min()
+    maxTime = accData[:, 0].max()
+    newTime, step = scipy.linspace(minTime, maxTime, num=len(accData[:, 0]), retstep = True)
+    for i in range(3):
+        accData[:, i + 1] = np.interp(newTime, accData[:, 0], accData[:, i + 1])
+    accData[:, 0] = newTime
+
+    # apply fft transform
+    for i in range(3):
+        FFT = abs(scipy.fft(accData[:, i + 1]))
+        #freqs = scipy.fftpack.fftfreq(len(newTime), step)
+        #accData[:, 0] = freqs
+        accData[:, i + 1] = scipy.log10(FFT)
+
+    gyro = [];
+    for i in range(3):
+        # Convert gyrometer data to radians
+        gyro.append(gyroData[:, i + 1] * 2 * math.pi)
+        # Integrate data for approximate location
+        gyro[i] = integrate.cumtrapz(gyro[i], gyroData[:, 0], initial=0)
+        # apply high pass filter to reduce drift
+        # and we don't care about overall change in motion
+        #
+        
+        # interpolate data with accelerometer data
+        gyro[i] = np.interp(accData[:, 0], gyroData[:, 0], gyro[i])
+
+    gyroData = np.column_stack((accData[:, 0], gyro[0], gyro[1], gyro[2]))
     # interpolate gyro data
-    xGyro = np.interp(accData[:, 0], gyroData[:, 0], gyroData[:, 1])
     yGyro = np.interp(accData[:, 0], gyroData[:, 0], gyroData[:, 2])
     zGyro = np.interp(accData[:, 0], gyroData[:, 0], gyroData[:, 3])
-    gyroData = np.column_stack((accData[:, 0], xGyro, yGyro, zGyro))
+    
 
-    # calculate absolute reference
 
-    return gyroData
+    return (accData, gyroData)
